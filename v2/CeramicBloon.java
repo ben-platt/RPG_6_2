b1c368777f09cde2fb661a900e2aea1ce2255703
gshey <gshey@stuy.edu>
2017-12-05 21:57:18 -0500

    monster subcategories added

---
+++ v2/CeramicBloon.java
@@ -0,0 +1,7 @@
+public class CeramicBloon extends Monster{
+  public CeramicBloon(){
+    super();
+    HP = 300;
+    defense = 40;
+  }
+}
---
+++ v2/Wolf.java
@@ -0,0 +1,7 @@
+public class Wolf extends Monster{
+  public Wolf(){
+    super();
+    HP = 50;
+    strength = 10;
+  }
+}
